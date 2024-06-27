5530ba7a9bd1d313fbd39b704a5b590ec4501878
Eleonilson Rodrigues <eleonilson.r.l@gmail.com>
2024-06-26 21:39:22 -0300

    ComboDevaice

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,27 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretPrinter;
+import devices.ConcretScanner;
 
 public class Program {
 
   public static void main(String[] args) {
     
-      Printer p = new Printer("1080");
+      ConcretPrinter p = new ConcretPrinter("1080");
       p.processDoc("My letter");
       p.print("My letter");
 
-      Scanner s = new Scanner("2003");
+      System.out.println();
+      ConcretScanner s = new ConcretScanner("2003");
       s.processDoc("My email");
       System.out.println("Scan result: " + s.scan());
+
+      System.out.println();
+      ComboDevice c = new ComboDevice("2081");
+      c.processDoc("May dissertation");
+      c.print("My dissertation");
+      System.out.println("Scan result: " + c.scan());
   }
     
 }
--- src/devices/Scanner.java
+++ src/devices/Scanner.java
@@ -1,20 +1,5 @@
 package devices;
 
-public class Scanner extends Device {
-
-    public Scanner(String serialNumber) {
-      super(serialNumber);
-    }
-
-    @Override
-    public void processDoc(String doc) {
-        System.out.println("Scanner processing: " + doc);
-    }
-
-    public String scan() {
-      return "Scanning content";
-    }
-
-    
-  
+public interface Scanner {
+    String scan();
 }
